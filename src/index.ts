b4f1a4a089ec3e43402601a1c6edded92c025314
aufabdulnafea <auf.abdulnafea@gmail.com>
2026-06-25 05:17:33 +0200

    added currency, customerIpAddress, isBusinessCustomer and customerTaxId to CheckoutParams

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+export { polarClient } from "./client";
+export { polar } from "./server";
+
+export * from "./plugins/portal";
+export * from "./plugins/checkout";
+export * from "./plugins/usage";
+export * from "./plugins/webhooks";
